7842ae2106960cb84105762e603d8b5e2ad8d61f
Brian Kim <s3kim2018@berkeley.edu>
2020-12-27 18:02:56 -0800

    added sematic library

--- static/index.js
+++ static/index.js
@@ -7,8 +7,11 @@ let contact = document.getElementById('contact')
 console.log("loaded")
 home.style.backgroundSize = '12% 0.1em';
 home.style.color = 'blue';
+document.getElementById('homepage').style.opacity = '1'; 
+document.getElementById('homepage').style.height = '100%';
 
 let currentelem = 'home'
+let pageelem = currentelem + 'page'; 
 function changemenu(val) {
     if (val == currentelem) {
         return;
@@ -16,22 +19,35 @@ function changemenu(val) {
         if (val == 'home') {
             home.style.backgroundSize = '12% 0.1em';
             home.style.color = 'blue';
+            document.getElementById('homepage').style.opacity = '1'; 
+            document.getElementById('homepage').style.height = '100%';
         } else if (val == 'about') {
             about.style.backgroundSize = '20% 0.1em';
             about.style.color = 'blue';
+            document.getElementById('aboutpage').style.opacity = '1'; 
+            document.getElementById('aboutpage').style.height = '100%';
         } else if (val == 'blog') {
             blog.style.backgroundSize = '10% 0.1em';
             blog.style.color = 'blue';
+            document.getElementById('blogpage').style.opacity = '1';
+            document.getElementById('blogpage').style.height = '100%';
         } else if (val == "projects") {
             projects.style.backgroundSize = '16% 0.1em';
             projects.style.color = 'blue';
+            document.getElementById('projectspage').style.opacity = '1';
+            document.getElementById('projectspage').style.height = '100%';
         } else if (val == 'contact') {
             contact.style.backgroundSize = '22% 0.1em';
             contact.style.color = 'blue';
+            document.getElementById('contactpage').style.opacity = '1';
+            document.getElementById('contactpage').style.height = '100%';
         }
         document.getElementById(currentelem).style.backgroundSize = '0% 0.1em';
         document.getElementById(currentelem).style.color = 'black';
+        document.getElementById(pageelem).style.opacity = '0';
+        document.getElementById(pageelem).style.height = '0%' 
         currentelem = val
+        pageelem = currentelem + 'page'; 
     }
 }
 home.addEventListener('click', function() {
